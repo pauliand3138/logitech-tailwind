606dec1a31129b12a765e640412e6d541ecd3034
Paul <paul.iand3138@hotmail.com>
2023-11-19 22:41:11 +0800

    Update Button effect

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -10,8 +10,9 @@ const PopularProducts = () => {
                     Products
                 </h2>
                 <p className="lg-max-w-lg mt-2 font-montserrat text-slate-gray">
-                    Experience top-notch quality and style with our sought-after
-                    selections. Discover a world of comfort, design and value
+                    Indulge in top-notch quality and style with our sought-after
+                    mice. Experience a world of comfort, sleek design, and
+                    unbeatable value.
                 </p>
             </div>
             <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
